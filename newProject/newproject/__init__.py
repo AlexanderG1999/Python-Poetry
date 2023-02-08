e1b6b53ba19fee108342367bfb0766e988983d0e
AlexanderG1999 <95202925+AlexanderG1999@users.noreply.github.com>
2023-02-08 15:19:25 -0500

    First commit

---
+++ newProject/newproject/__init__.py
@@ -0,0 +1,22 @@
+from plyer import notification
+import time
+
+def inputtime():
+	hour = int(input("hours of interval :"))
+	minutes = int(input("Mins of interval :"))
+	seconds = int(input("Secs of interval :"))
+	time_interval = seconds+(minutes*60)+(hour*3600)
+	return time_interval
+
+def notify():
+	notification.notify(
+		title = "Time is Up",
+		message="Hello it is an example with poetry-python" ,
+			
+		timeout=5
+	)
+
+if __name__ == '__main__':
+	time_interval = inputtime()
+	time.sleep(time_interval)
+	notify()
